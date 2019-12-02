1e7be37b72960f3b152e5aeb2bd048139d443560
Eyitope Adelowo <topeboy2009@gmail.com>
2019-12-02 15:13:01 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,11 +3,16 @@
 #include <TinyGPS++.h> 
 #include <SoftwareSerial.h>
 
+
 extern char first_transmit;
 
-extern int status_led;
-extern int tilt_sensor;
-extern int arm_button;
+extern const int status_led;
+extern const int tilt_sensor;
+extern const int arm_button;
+
+extern float celsius;
+extern float lattitude;
+extern float longitude;
 
 extern String current_time;
 extern String tilt_time;
@@ -21,13 +26,12 @@ void inline fetch_time(String time_);
 void setup_rtc(void);
 void setup_temp_sensor(void);
 void setup_gsm(void);
-void format_data(void);
 bool transmit_data();   //defined in 3rd file
 void smartDelay(unsigned long ms);   //GPS function
 char *ftoa(char *a, double f, int precision);   //float to integer
 
 extern SoftwareSerial gps_uart;
-extern SoftwareSerial gsm;
+//extern SoftwareSerial gsm;
 extern OneWire temp_sensor;
 extern RTC_DS3231 rtc;
 extern TinyGPSPlus gps;
@@ -37,13 +41,13 @@ void setup()
   pinMode(status_led, OUTPUT);
   pinMode(tilt_sensor, INPUT);
   pinMode(arm_button, INPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
   gps_uart.begin(9600);
-  gsm.begin(115200);
+  //gsm.begin(115200);
   setup_temp_sensor();
   setup_rtc();
 
-  Serial.println("Waiting for button");
+  Serial.println(F("Waiting for button"));
   while(digitalRead(arm_button) == HIGH) {    //Wait till system is armed
     blink_(100);
   }
@@ -52,8 +56,6 @@ void setup()
   start_time = String(now.year())+'/'+ String(now.month())+'/'+ String(now.day())+ " "+ String(now.hour())+':'+
                String(now.minute())+':'+ String(now.second());
   first_transmit = '1';
-  //transmit_data();
-  first_transmit = '0'; 
 
   Serial.println("\nLOOP STARTS");
 }
@@ -63,15 +65,14 @@ void setup()
 void loop() 
 {
   unsigned long time_now = millis();
+  first_transmit = '0';
   while((millis() - time_now  <= 10000))  {   //Every 5 mins, but 10s for testing
     if(digitalRead(tilt_sensor) == HIGH){
       digitalWrite(status_led, HIGH);
       DateTime now = rtc.now();
       tilt_time = String(now.hour())+ ':' + String(now.minute())+ ':'+
                   String(now.second());  
-      format_data();
-      transmit_data();
-      tilt_time = " ";   //reset after transmitting
+      //transmit data if tilted here
       digitalWrite(status_led, LOW);
     }
     blink_(500);
@@ -82,12 +83,22 @@ void loop()
   DateTime now = rtc.now();
   current_time = String(now.hour())+ ':' + String(now.minute())+ ':'+
               String(now.second());
-  format_data();
+              
+//  DATA = '{' +
+//        "\"temperature\":" + String(celsius,2) + "," + 
+//        "\"longitude\":" + String(longitude,4) + "," +
+//        "\"latitude\":" + String(lattitude,4) + "," +
+//        "\"current_time\":" + current_time + "," +
+//        "\"tilt_time\":" + tilt_time + "," +
+//        "\"first_transmit\":" + first_transmit + "," + 
+//        "\"start_time\":" + start_time +
+//        '}';
+//        
   transmit_data();
   digitalWrite(status_led, LOW); 
-
-  Serial.println("DATA = "); Serial.print(DATA); Serial.println('\n');
+  //  Serial.println("DATA = "); Serial.print(DATA); Serial.println('\n');
 }
+
 
 
 void blink_(int time_) 
